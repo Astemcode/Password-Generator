434978ae70065add2e75432f88716579b3f911c4
Joivre Charles <astemcode@gmail.com>
2020-10-16 21:27:20 -0400

    Update script.js
    Fixed random password generator, I was able to add the screen prompts for the clients input.

--- script.js
+++ script.js
@@ -10,15 +10,45 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_-+=<>,.?",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+// Random Password Generator  
+const key_strings = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    number: '0123456789',
+    symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  };
+  
+  function generatePassword() {
+    var passwordCharSet = "";
+  
+    var length = window.prompt("Enter a number from 8 to 128 for password length.");
+  
+    var lowercase = window.confirm("Would you like to use lowercase letters?");
+    if (lowercase) {
+      passwordCharSet += key_strings.lowercase;
+    };
+  
+    var uppercase = window.confirm("Would you like to use uppercase letters?");
+    if (uppercase) {
+      passwordCharSet += key_strings.uppercase;
+    };
+  
+    var symbols = window.confirm("Would you like to use symbols?");
+    if (symbols) {
+      passwordCharSet += key_strings.symbol;
+    };
+  
+    var numbers = window.confirm("Would you like to use numbers?");
+    if (numbers) {
+      passwordCharSet += key_strings.number;
+    };
+    var password = "";
+    for (let i = 0; i < length; i++) {
+      password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
     }
-    return retVal;
-}
+    return password;
+  }
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
