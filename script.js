4725f87669a91ff55f479723590b1e5a8dc5f285
Joivre Charles <astemcode@gmail.com>
2020-10-16 21:53:55 -0400

    Page Update
    Changed the background image of password generator. Updated the title font color to contrast background. Updated the prompt responses and string names

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function writePassword() {
 }
 
 // Random Password Generator  
-const key_strings = {
+const random_strings = {
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     number: '0123456789',
@@ -25,22 +25,22 @@ const key_strings = {
   
     var lowercase = window.confirm("Would you like to use lowercase letters?");
     if (lowercase) {
-      passwordCharSet += key_strings.lowercase;
+      passwordCharSet += random_strings.lowercase;
     };
   
     var uppercase = window.confirm("Would you like to use uppercase letters?");
     if (uppercase) {
-      passwordCharSet += key_strings.uppercase;
+      passwordCharSet += random_strings.uppercase;
     };
   
     var symbols = window.confirm("Would you like to use symbols?");
     if (symbols) {
-      passwordCharSet += key_strings.symbol;
+      passwordCharSet += random_strings.symbol;
     };
   
     var numbers = window.confirm("Would you like to use numbers?");
     if (numbers) {
-      passwordCharSet += key_strings.number;
+      passwordCharSet += random_strings.number;
     };
     var password = "";
     for (let i = 0; i < length; i++) {
